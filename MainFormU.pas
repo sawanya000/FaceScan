0a7fc932e5d3eca0f657883bd23bc8958dcc2b27
sawanya000 <sawanya000@gmail.com>
2020-07-02 17:19:49 +0700

    test

--- MainFormU.pas
+++ MainFormU.pas
@@ -4,10 +4,12 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TForm21 = class(TForm)
+    Button1: TButton;
   private
     { Private declarations }
   public
